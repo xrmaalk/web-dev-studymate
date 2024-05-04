f6f1dd99fda99dcb819cdc22baaecb373e08888b
xrmaalk <xinra.inc@gmail.com>
2024-05-04 12:17:21 -0600

    Update Question array

--- src/index.js
+++ src/index.js
@@ -1,58 +1,57 @@
 //---------- GLOBAL VARIABLES -------------------//
-let currentQuestionIndex = 0;
-let selectedCategoryQuestions;
-let availableQuestions;
-const typingSpeed = 50; //typing ( milliseconds)
+let currentQuestionIndex = 0
+let selectedCategoryQuestions
+let availableQuestions
+const typingSpeed = 50 //typing ( milliseconds)
 
-let questions;
-let speed = 50;
-let speechOn = true;
-let i = 0;
+let questions
+let speed = 50
+let speechOn = true
+let i = 0
 let txt =
   ("Welcome.....",
   "I'm Turtee...",
   "your study buddy!",
-  "Choose a category to begin....");
+  "Choose a category to begin....")
 
 //----------------- INPUT DATA -----------------//
 
 async function fetchQuestions() {
-  const response = await fetch("../data/inputData.json");
-  const questions = await response.json();
-  // s
-  return questions;
+  const response = await fetch("../data/inputData.json")
+  const questions = await response.json()
+
+  return questions
 }
 
 function answerWriter() {
   if (i < txt.length) {
-    document.getElementById("flashcard-container").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(answerWriter, speed);
+    document.getElementById("flashcard-container").innerHTML += txt.charAt(i)
+    i++
+    setTimeout(answerWriter, speed)
   }
 }
 
 //------------- START GAME ----------------//
 async function startGame(category) {
   if (!availableQuestions) {
-    availableQuestions = await fetchQuestions();
+    availableQuestions = await fetchQuestions()
   }
 
   if (availableQuestions.hasOwnProperty(category)) {
-    selectedCategoryQuestions = availableQuestions[category];
+    selectedCategoryQuestions = availableQuestions[category]
 
-    document.getElementById("flashcard-container").style.display =
-      "inline-flex";
-    document.getElementById("navigation").style.display = "flex";
+    document.getElementById("flashcard-container").style.display = "inline-flex"
+    document.getElementById("navigation").style.display = "flex"
     // document.getElementById("content-container").style.display = "none";
 
     if (selectedCategoryQuestions.length > 0) {
-      updateQuestionDisplay();
-      hideButton(category);
+      updateQuestionDisplay()
+      hideButton(category)
     } else {
-      console.error(`No questions available for the category: ${category}`);
+      console.error(`No questions available for the category: ${category}`)
     }
   } else {
-    console.error(`Category not found: ${category}`);
+    console.error(`Category not found: ${category}`)
   }
 }
 
@@ -60,13 +59,13 @@ async function startGame(category) {
 
 //------------ QUESTION AND ANSWERS -------------//
 function updateQuestionDisplay() {
-  const currentQuestion = selectedCategoryQuestions[currentQuestionIndex];
+  const currentQuestion = selectedCategoryQuestions[currentQuestionIndex]
 
-  console.log(currentQuestionIndex, selectedCategoryQuestions.length - 1);
+  console.log(currentQuestionIndex, selectedCategoryQuestions.length - 1)
 
   // currentQuestion.answer;
 
-  const questionContainer = document.getElementById("flashcard-container");
+  const questionContainer = document.getElementById("flashcard-container")
   questionContainer.innerHTML = `
     <h2 id="h2-tag">${currentQuestion.question}</h2>
     <div id="p-tag">${currentQuestion.answer}</div>
@@ -75,67 +74,67 @@ function updateQuestionDisplay() {
         <image id="facing-turtee-img" xlink:href="./img/SVG/Asset_8_walking.svg" />
       </svg>
     </div>
-  `;
+  `
   if (
     (currentQuestionIndex + 1 == selectedCategoryQuestions.length - 1,
     console.log(currentQuestionIndex, selectedCategoryQuestions.length))
   ) {
-    currentQuestionIndex = 0;
-    resetGame();
+    currentQuestionIndex = 0
+    resetGame()
   }
 }
 
 //----------- NAVIGATE Q & A --------------------//
 
 function displayAnswer() {
-  const answerContainer = document.getElementById("flashcard-container");
-  document.getElementById("p-tag").style.display = "flex";
-  document.getElementById("h2-tag").style.display = "none";
-  const currentQuestion = selectedCategoryQuestions[currentQuestionIndex];
-  answerContainer.innerHTML = `<p">${currentQuestion.answer}</>`;
-  document.getElementById("turtee-explain").style.display = "flex";
-  document.getElementById("pre-footer-id").style.display = "flex";
-  playSoundEffect();
+  const answerContainer = document.getElementById("flashcard-container")
+  document.getElementById("p-tag").style.display = "flex"
+  document.getElementById("h2-tag").style.display = "none"
+  const currentQuestion = selectedCategoryQuestions[currentQuestionIndex]
+  answerContainer.innerHTML = `<p">${currentQuestion.answer}</>`
+  document.getElementById("turtee-explain").style.display = "flex"
+  document.getElementById("pre-footer-id").style.display = "flex"
+  playSoundEffect()
 }
 
 function previousQuestion() {
-  document.getElementById("turtee-explain").style.display = "none";
+  document.getElementById("turtee-explain").style.display = "none"
   if (currentQuestionIndex > 0) {
-    currentQuestionIndex--;
-    updateQuestionDisplay();
+    currentQuestionIndex--
+    updateQuestionDisplay()
   } else {
-    document.getElementById("prev-button").style.cursor = "not-allowed";
+    document.getElementById("prev-button").style.cursor = "not-allowed"
   }
 }
 
 function nextQuestion() {
   if (currentQuestionIndex < selectedCategoryQuestions.length - 1) {
-    currentQuestionIndex++;
-    updateQuestionDisplay();
+    currentQuestionIndex++
+    updateQuestionDisplay()
   } else {
-    document.getElementById("next-button").style.cursor = "not-allowed";
+    document.getElementById("next-button").style.cursor = "not-allowed"
   }
 }
 //----------- CHANGE NAVIGATION ------------------//
 
 function updateNavigationButtons() {
-  const prevButton = document.getElementById("prev-button");
-  const answerButton = document.getElementById("answer-button");
-  const nextButton = document.getElementById("next-button");
+  const prevButton = document.getElementById("prev-button")
+  const answerButton = document.getElementById("answer-button")
+  const nextButton = document.getElementById("next-button")
 
   // Disable/enable prevButton and set cursor accordingly
-  const isFirstQuestion = currentQuestionIndex === 0;
-  prevButton.disabled = isFirstQuestion;
-  prevButton.style.cursor = isFirstQuestion ? "not-allowed" : "pointer";
+  const isFirstQuestion = currentQuestionIndex === 0
+  prevButton.disabled = isFirstQuestion
+  prevButton.style.cursor = isFirstQuestion ? "not-allowed" : "pointer"
 
   // Enable answerButton
-  answerButton.disabled = false;
+  answerButton.disabled = false
 
   // Disable/enable nextButton and set cursor accordingly
   const isLastQuestion =
-    currentQuestionIndex === categories[currentCategory].length - 1;
-  nextButton.disabled = isLastQuestion;
-  nextButton.style.cursor = isLastQuestion ? "not-allowed" : "pointer";
+    currentQuestionIndex === categories[currentCategory].length - 1
+  nextButton.disabled = isLastQuestion
+  nextButton.style.cursor = isLastQuestion ? "not-allowed" : "pointer"
 }
 
 // function updateNavigationButtons() {
@@ -163,41 +162,41 @@ function updateNavigationButtons() {
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("cat-select").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
+    document.getElementById("cat-select").innerHTML += txt.charAt(i)
+    i++
+    setTimeout(typeWriter, speed)
   }
 }
 
 //------------ RESTART GAME ------------------//
 function resetGame() {
-  document.getElementById("logo-svg").style.display = "none";
-  document.getElementById("copy-right").style.display = "none";
-  document.getElementById("speech-bubble").style.display = "flex";
-  document.getElementById("flashcard-container").style.display = "none";
-  document.getElementById("navigation").style.display = "none";
+  document.getElementById("logo-svg").style.display = "none"
+  document.getElementById("copy-right").style.display = "none"
+  document.getElementById("speech-bubble").style.display = "flex"
+  document.getElementById("flashcard-container").style.display = "none"
+  document.getElementById("navigation").style.display = "none"
   if (availableQuestions === undefined) {
-    document.getElementById("reset").style.cursor = "not-allowed";
+    document.getElementById("reset").style.cursor = "not-allowed"
   } else {
-    document.getElementById("reset").style.cursor = "pointer";
-    document.getElementById("welcome").style.display = "hidden";
-    document.getElementById("category-button").style.visibility = "visible";
-    document.getElementById("pre-footer-id").style.visibility = "visible";
-    document.getElementById("css").style.display = "flex";
-    document.getElementById("js").style.display = "flex";
-    document.getElementById("html5").style.display = "flex";
-    document.getElementById("turtee-8").style.display = " flex";
-    document.getElementById("turtee-explain").style.display = "none";
+    document.getElementById("reset").style.cursor = "pointer"
+    document.getElementById("welcome").style.display = "hidden"
+    document.getElementById("category-button").style.visibility = "visible"
+    document.getElementById("pre-footer-id").style.visibility = "visible"
+    document.getElementById("css").style.display = "flex"
+    document.getElementById("js").style.display = "flex"
+    document.getElementById("html5").style.display = "flex"
+    document.getElementById("turtee-8").style.display = " flex"
+    document.getElementById("turtee-explain").style.display = "none"
 
-    document.getElementById("dragonHackerButton").style.display = "none";
-    document.getSelection("speech-bubble").style.display = "flex";
-    document.getSelection("content-container").style.display = "flex";
+    document.getElementById("dragonHackerButton").style.display = "none"
+    document.getSelection("speech-bubble").style.display = "flex"
+    document.getSelection("content-container").style.display = "flex"
   }
 }
 
 function playSoundEffect() {
-  let soundEffect = document.getElementById("sound-start");
-  soundEffect.play();
+  let soundEffect = document.getElementById("sound-start")
+  soundEffect.play()
 }
 
 function hideButton(clickedButtonId) {
@@ -209,47 +208,47 @@ function hideButton(clickedButtonId) {
     "speech-bubble",
     "logo-svg",
     "copy-right",
-  ];
+  ]
 
   const elementsToKeep = {
     html: ["css", "js"],
     css: ["html5", "js"],
     javascript: ["html5", "css"],
-  };
+  }
 
   elements.forEach((id) => {
-    const element = document.getElementById(id);
+    const element = document.getElementById(id)
     if (
       element &&
       (!elementsToKeep[clickedButtonId] ||
         !elementsToKeep[clickedButtonId].includes(id))
     ) {
-      element.style.display = "none";
+      element.style.display = "none"
     }
-  });
+  })
 
   if (!elementsToKeep.hasOwnProperty(clickedButtonId)) {
-    console.log(clickedButtonId);
+    console.log(clickedButtonId)
   }
   if (clickedButtonId === "html") {
-    document.getElementById("css").style.display = "none";
-    document.getElementById("js").style.display = "none";
+    document.getElementById("css").style.display = "none"
+    document.getElementById("js").style.display = "none"
   } else if (clickedButtonId === "css") {
-    document.getElementById("html5").style.display = "none";
-    document.getElementById("js").style.display = "none";
+    document.getElementById("html5").style.display = "none"
+    document.getElementById("js").style.display = "none"
   } else if (clickedButtonId === "javascript") {
-    document.getElementById("html5").style.display = "none";
-    document.getElementById("css").style.display = "none";
+    document.getElementById("html5").style.display = "none"
+    document.getElementById("css").style.display = "none"
   } else {
-    console.log(clickedButtonId);
+    console.log(clickedButtonId)
   }
 }
 
 // ---------------- LOGIC ----------------------//
 
 function introduceGame() {
-  setTimeout(typeWriter(), speed);
-  document.getElementById("speech-bubble").style.display = "flex";
+  setTimeout(typeWriter(), speed)
+  document.getElementById("speech-bubble").style.display = "flex"
 }
 
 function showStartMenu() {
@@ -261,30 +260,30 @@ function showStartMenu() {
     { id: "dragonHackerButton", display: "none" },
     { id: "turtee-8", display: "flex" },
     { id: "cat-select", display: "flex" },
-  ];
+  ]
 
   elements.forEach(({ id, display }) => {
-    const element = document.getElementById(id);
+    const element = document.getElementById(id)
     if (element) {
-      element.style.display = display;
+      element.style.display = display
     }
-  });
+  })
 
-  introduceGame();
+  introduceGame()
   // playSoundEffect();
 }
 function runCode() {
   // Get the code from the textarea
-  var code = document.getElementById("code-box").value;
+  var code = document.getElementById("code-box").value
 
   // Get the iframe
-  var iframe = document.getElementById("resultFrame");
+  var iframe = document.getElementById("resultFrame")
 
   // Write the code into the iframe document
-  var iframeDoc = iframe.contentWindow.document;
-  iframeDoc.open();
-  iframeDoc.write(code);
-  iframeDoc.close();
+  var iframeDoc = iframe.contentWindow.document
+  iframeDoc.open()
+  iframeDoc.write(code)
+  iframeDoc.close()
 }
 //------------ EXPERIMENTAL FEATURES -------------//
 // function resetGame() {
